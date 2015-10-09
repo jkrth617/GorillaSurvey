c57cc50752cf3d6f83c908ac4d9fa6373f4acae3
jake roth & Sarah O'Shea <pair+jkrth617.saraho-shea@devbootcamp.com>
2015-10-09 16:16:36 -0400

    added a user show page and then also fixed the forms to store the value of the (at) user so it filled in the fields if there was an error

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -13,3 +13,28 @@ post '/signup/' do
     erb :'user/signup'
   end
 end
+
+get '/login' do
+  erb :'user/login'
+end
+
+post '/login' do
+  user = User.find_by(user_name: params[:user_name])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    @errors = user.errors.full_messages
+    erb :'user/login'
+  end
+end
+
+get 'logout' do
+  session.clear
+  redirect "/"
+end
+
+get '/users/:id' do |user_id|
+  @user = User.find(user_id)
+  erb :'user/show'
+end
