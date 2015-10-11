3a50b94bb80fdb39a58e2b83f50051f94508c7c2
jake roth <email": null>
2015-10-11 15:38:12 -0400

    first sticking point

--- app/controllers/choice.rb
+++ app/controllers/choice.rb
@@ -1,14 +1,18 @@
 get '/questions/:id/choices/new' do |q_id|
   @choice = Choice.new
   @question = Question.find(q_id)
-  flash[:message] = "choice saved!!"
-  erb :'choice/new'
+  if request.xhr?
+    erb :'choice/_new-form', layout: false, locals: {question: @question, choice: @choice}
+  else
+    erb :'choice/new'
+  end
 end
 
 post '/questions/:id/choices' do |q_id|
   @question = Question.find(q_id)
   @choice = @question.choices.new(params[:choice])#make sure this alligns with the erb's form
   if @choice.save
+    flash[:message] = "choice saved!!"
     redirect "/questions/#{@question.id}/choices/new"
   else
     @errors = @choice.errors.full_messages
--- public/js/application.js
+++ public/js/application.js
@@ -1,19 +1,36 @@
 $(document).ready(function() {
 
-  $('#new_survey_form').on('click', function(event){
+  $('#new_survey_form').on('submit', function(event){
     event.preventDefault();
-    var method = this.attr('method');
-    var target_url = this.attr('action');
-    var form_data = this.target.serialize();
+    // debugger;
+    var method = $(this).attr('method');
+    var target_url = $(this).attr('action');
+    var form_data = $(this).serialize();
     $.ajax({
       type: method,
       url: target_url,
       data: form_data
     }).done(function(response){
+      $('#question-containter').append(response)
+    })
+    // no fail and also no hide. now whenever you click that button another question will be appended to the bottom
+  })
 
+  $('#question-containter').on('submit', '#new_question_form', function(event){
+    event.preventDefault();
+    var method = $(this).attr('method');
+    var target_url = $(this).attr('action');
+    var form_data = $(this).serialize();
+    $.ajax({
+      type: method,
+      url: target_url,
+      data: form_data
+    }).done(function(response){
+      $('#question-containter').append(response)
     }).fail(function(deffered){
 
     })
   })
+
 
 });
