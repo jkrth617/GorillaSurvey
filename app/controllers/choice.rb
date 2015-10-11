81a8e56ad33620bf4296bbd372f3ece0873c14aa
jake roth <email": null>
2015-10-11 12:39:14 -0400

    another typo in the route found

--- app/controllers/choice.rb
+++ app/controllers/choice.rb
@@ -1,14 +1,14 @@
-get '/questions/:id/choice/new' do |q_id|
+get '/questions/:id/choices/new' do |q_id|
   @choice = Choice.new
   @question_id = q_id
   erb :'choice/new'
 end
 
-post '/questions/:id/choice' do |q_id|
+post '/questions/:id/choices' do |q_id|
   question = Question.find(q_id)
   @choice = question.choices.new(params[:choice])#make sure this alligns with the erb's form
   if @choice.save
-    redirect "/questions/:id/choice/new"
+    redirect "/questions/:id/choices/new"
   else
     @errors = @choice.errors.full_messages
     erb :'choice/new'
