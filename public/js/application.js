03e161e08e9d9513b52fd04a10669c3d60b3e50d
jake roth <email": null>
2015-10-11 17:39:35 -0400

    done for now...

--- public/js/application.js
+++ public/js/application.js
@@ -12,12 +12,9 @@ $(document).ready(function() {
       url: target_url,
       data: form_data
     }).done(function(response){
-      // debugger;
       $('#question-containter').append(response);
-      // $('#survey-title-submit').hide();
+      $('#survey-title-submit').hide();
       $('#save-all').show();
-      // $('#add-question').html($('#survey-title-submit'));
-
     })
     // no fail and also no hide. now whenever you click that button another question will be appended to the bottom
   })
@@ -32,24 +29,23 @@ $(document).ready(function() {
       url: target_url,
       data: form_data
     }).done(function(response){
-      $('#question-containter').append(response)
+      $('#question-containter').append(response);
       $('.question-save').hide();
-    }).fail(function(deffered){
-
-    })
+    }).fail(function(deffered){})
   })
 
 $('#question-containter').on('submit', '#add-question-form', function(event){
     event.preventDefault();
+    var button = $(this);
     var method = $(this).attr('method');
     var target_url = $(this).attr('action');
     $.ajax({
       type: method,
       url: target_url,
-      data: form_data
     }).done(function(response){
+      button.hide();
       $('#question-containter').append(response)
-    })
+    }).fail(function(deffered){})
   })
 
   $('#question-containter').on('submit', '#new_choice_form', function(event){
@@ -64,8 +60,6 @@ $('#question-containter').on('submit', '#add-question-form', function(event){
     }).done(function(response){
       $('.choice-save').hide();
       $('#question-containter').append(response)
-
-
     }).fail()
   })
 
