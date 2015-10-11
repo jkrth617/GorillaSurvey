8b8758e5f00a132ae928e41e1600779d20e2b489
jake roth <email": null>
2015-10-11 12:41:08 -0400

    changed the choice routing to simplyfiy the form in the erb

--- app/controllers/choice.rb
+++ app/controllers/choice.rb
@@ -1,11 +1,11 @@
 get '/questions/:id/choices/new' do |q_id|
   @choice = Choice.new
-  @question_id = q_id
+  @question = Question.find(q_id)
   erb :'choice/new'
 end
 
 post '/questions/:id/choices' do |q_id|
-  question = Question.find(q_id)
+  @question = Question.find(q_id)
   @choice = question.choices.new(params[:choice])#make sure this alligns with the erb's form
   if @choice.save
     redirect "/questions/:id/choices/new"
