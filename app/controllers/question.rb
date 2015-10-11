9bd2e1f9abf9152cfa3d1bbdabc98210f17da962
jake roth <email": null>
2015-10-11 11:27:45 -0400

    added the post for the question. it redirects to a choice maker after a sucessful save. didnt make the erb file for it

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,7 +1,16 @@
-get '/surveys/:id/questions/new' |survey_id|
+get '/surveys/:id/questions/new' do |survey_id|
   @question = Question.new
   @survey_id = survey_id
   erb :'question/new'
 end
 
-
+post '/surveys/:id/questions' do |survey_id|
+  survey = Survey.find(survey_id)
+  @question = survey.questions.new(params[:question])
+  if @question.save
+    redirect "/questions/#{@question.id}/choices/new"
+  else
+    @errors = @question.errors.full_messages
+    erb :'question/new'
+  end
+end
