76b8724061433e41bf459438a11e527ba9894c0d
jake roth <email": null>
2015-10-11 11:43:36 -0400

    added the choice post route

--- app/controllers/choice.rb
+++ app/controllers/choice.rb
@@ -4,4 +4,13 @@ get '/questions/:id/choice/new' do |q_id|
   erb :'choice/new'
 end
 
-
+post '/questions/:id/choice/new' do |q_id|
+  question = Question.find(q_id)
+  @choice = question.choices.new(params[:choice])#make sure this alligns with the erb's form
+  if @choice.save
+    redirect "/questions/:id/choice/new"
+  else
+    @errors = @choice.errors.full_messages
+    erb :'choice/new'
+  end
+end
