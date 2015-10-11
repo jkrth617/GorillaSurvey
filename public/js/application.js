9b80ee16e35b77743c64ba7cabc3a6d29125398c
jake roth <email": null>
2015-10-11 14:42:19 -0400

    started the ajax request, prevented default and starting to work with data

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#new_survey_form').on('click', function(event){
+    event.preventDefault();
+    var method = this.attr('method');
+    var target_url = this.attr('action');
+    var form_data = this.target.serialize();
+    $.ajax({
+      type: method,
+      url: target_url,
+      data: form_data
+    }).done(function(response){
+
+    }).fail(function(deffered){
+
+    })
+  })
+
 });
