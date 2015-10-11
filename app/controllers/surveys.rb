5bbc8a02004d82ef968ca07c0d27227726be7cde
AgentWang007 <jaybwang007@gmail.com>
2015-10-11 12:02:20 -0400

    fix rebase issue with show.erb and survey.rb controller

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -12,6 +12,7 @@ get '/surveys/:id' do
   erb :'survey/show'
 end
 
+
 post '/surveys' do
   @survey = Survey.find(params[:survey])
     if @survey.save
@@ -19,6 +20,10 @@ post '/surveys' do
     else
       erb :'survey/new'
   end
+
+post '/surveys/new' do
+
+
 end
 
 
