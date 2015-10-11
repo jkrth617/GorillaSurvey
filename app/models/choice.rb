4cdb957b4040f4aa00d2d26248c0a96e3425cccd
jake roth <email": null>
2015-10-11 10:30:55 -0400

    added the methods to the question and choice models to be able to get a choice's percentage selected as a string

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,4 +1,15 @@
 class Choice < ActiveRecord::Base
   belongs_to :question
   has_many :responses
+
+  def times_selected
+    self.responses.size
+  end
+
+  def percentage_selected
+    decimal = (self.times_selected.to_f)/(self.question.total_responses)
+    percent = decimal.round(4)*100
+    percent.to_s + "%"
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,12 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :choices
+
+  def total_responses
+    choice_array = self.choices
+    choice_array.inject(0) do |sum, choice|
+      sum + choice.times_selected
+    end
+  end
+
 end
